b3f8ca68e14edaeab5f08cb03eafb0d7a5ae2b23
Eben <me@ebenroux.co.za>
2022-04-26 16:12:28 +0200

    deploy

--- assets/components_form.md.5975c62b.lean.js
+++ assets/components_form.md.5975c62b.lean.js
@@ -1,11 +1,10 @@
-import { _ as _sfc_main$4 } from "./chunks/Button.5598b7d0.js";
+import { o as openBlock, b as createElementBlock, j as renderSlot, n as normalizeClass, e as ref, a as createVNode, u as unref, c as createBlock, d as createCommentVNode, k as withCtx, H as withModifiers, f as createBaseVNode, g as createTextVNode, h as createStaticVNode } from "./app.de3fe290.js";
+import { _ as _sfc_main$3 } from "./chunks/Alert.1b1f4621.js";
+import { _ as _sfc_main$5 } from "./chunks/Button.5598b7d0.js";
 import { _ as _sfc_main$2 } from "./chunks/ButtonGroup.b53b94ea.js";
-import { o as openBlock, b as createElementBlock, j as renderSlot, n as normalizeClass, H as withModifiers, e as ref, a as createVNode, u as unref, k as withCtx, f as createBaseVNode, g as createTextVNode, h as createStaticVNode } from "./app.de3fe290.js";
 import { u as useCoreClass } from "./chunks/useCoreClass.4c1eebb8.js";
-import { _ as _sfc_main$3 } from "./chunks/Input.bc0d53ea.js";
+import { _ as _sfc_main$4 } from "./chunks/Input.bc0d53ea.js";
 import "./chunks/useAlert.8478351b.js";
-import "./chunks/Alert.1b1f4621.js";
-const _hoisted_1$1 = ["onSubmit"];
 const _sfc_main$1 = {
   props: {
     size: {
@@ -16,12 +15,8 @@ const _sfc_main$1 = {
       type: Object
     }
   },
-  emits: ["submit"],
-  setup(__props, { emit }) {
+  setup(__props) {
     const props = __props;
-    const submit = () => {
-      emit("submit");
-    };
     const getOptions = (include) => {
       return {
         svClass: props.svClass,
@@ -39,11 +34,10 @@ const _sfc_main$1 = {
     };
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock("form", {
-        class: normalizeClass(getClasses()),
-        onSubmit: withModifiers(submit, ["prevent"])
+        class: normalizeClass(getClasses())
       }, [
         renderSlot(_ctx.$slots, "default")
-      ], 42, _hoisted_1$1);
+      ], 2);
     };
   }
 };
@@ -70,7 +64,7 @@ const _hoisted_2 = /* @__PURE__ */ createBaseVNode("h2", {
   }, "#")
 ], -1);
 const _hoisted_3 = /* @__PURE__ */ createTextVNode("Submit");
-const _hoisted_4 = /* @__PURE__ */ createStaticVNode('<h2 id="properties" tabindex="-1">Properties <a class="header-anchor" href="#properties" aria-hidden="true">#</a></h2><table><thead><tr><th>Name</th><th>Type</th><th>Default</th><th>Description</th></tr></thead><tbody><tr><td><code>size</code></td><td><code>String</code></td><td><code>full</code></td><td>The size of the form:<br>- <code>small</code><br>- <code>medium</code><br>- <code>large</code><br>- <code>full</code></td></tr><tr><td><code>sv-class</code></td><td><code>Object</code></td><td><code>{}</code></td><td>The <a href="/shuttle-vue3/components/core-class.html">core class object</a> that will render classes along with the corresponding BEM entries.</td></tr></tbody></table><h2 id="events" tabindex="-1">Events <a class="header-anchor" href="#events" aria-hidden="true">#</a></h2><table><thead><tr><th>Name</th><th>Description</th></tr></thead><tbody><tr><td><code>submit</code></td><td>Called when the form is submitted (enter is pressed within the form).</td></tr></tbody></table><h2 id="classes" tabindex="-1">Classes <a class="header-anchor" href="#classes" aria-hidden="true">#</a></h2><ul><li><code>sv-form</code><ul><li><code>sv-form--small</code></li><li><code>sv-form--medium</code></li><li><code>sv-form--large</code></li></ul></li></ul>', 6);
+const _hoisted_4 = /* @__PURE__ */ createStaticVNode("", 6);
 const __pageData = '{"title":"Form","description":"","frontmatter":{},"headers":[{"level":2,"title":"Examples","slug":"examples"},{"level":2,"title":"Properties","slug":"properties"},{"level":2,"title":"Events","slug":"events"},{"level":2,"title":"Classes","slug":"classes"}],"relativePath":"components/form.md"}';
 const __default__ = {};
 const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
@@ -94,6 +88,12 @@ const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
         value: "full"
       }
     ]);
+    const message = ref("");
+    const count = ref(0);
+    const submitted = () => {
+      count.value++;
+      message.value = `Form submission count: ${count.value}`;
+    };
     return (_ctx, _cache) => {
       return openBlock(), createElementBlock("div", null, [
         _hoisted_1,
@@ -104,16 +104,26 @@ const _sfc_main = /* @__PURE__ */ Object.assign(__default__, {
           buttons: buttons.value,
           class: "mb-4"
         }, null, 8, ["modelValue", "buttons"]),
+        !!message.value ? (openBlock(), createBlock(unref(_sfc_main$3), {
+          key: 0,
+          variant: "info",
+          message: message.value,
+          class: "mb-4"
+        }, null, 8, ["message"])) : createCommentVNode("", true),
         createVNode(unref(_sfc_main$1), {
           class: "border border-solid border-gray-600 p-2 rounded",
-          size: size.value
+          size: size.value,
+          onSubmit: _cache[1] || (_cache[1] = withModifiers(($event) => submitted(), ["prevent"]))
         }, {
           default: withCtx(() => [
-            createVNode(unref(_sfc_main$3), {
+            createVNode(unref(_sfc_main$4), {
               label: "Input label",
               class: "pb-2"
             }),
-            createVNode(unref(_sfc_main$4), { class: "pt-2" }, {
+            createVNode(unref(_sfc_main$5), {
+              class: "pt-2",
+              type: "submit"
+            }, {
               default: withCtx(() => [
                 _hoisted_3
               ]),
