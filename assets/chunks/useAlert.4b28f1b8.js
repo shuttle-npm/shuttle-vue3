d5287ac23970c13a7dc92b1ad5b684614a6d5bfd
Eben <me@ebenroux.co.za>
2022-05-19 19:37:48 +0200

    deploy

---
+++ assets/chunks/useAlert.4b28f1b8.js
@@ -0,0 +1,13 @@
+import { f as computed } from "../app.e404c012.js";
+function useAlert(props) {
+  return computed(() => {
+    var _a;
+    if (!props.alert || !props.alert.message) {
+      return;
+    }
+    var result = props.alert;
+    result.variant = (_a = result.variant) != null ? _a : "info";
+    return result;
+  });
+}
+export { useAlert as u };
